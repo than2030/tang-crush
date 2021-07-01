01e81ceb1b17d854ff4f3624cce1c49b34d78f83
than2030 <86755337+than2030@users.noreply.github.com>
2021-07-01 09:44:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'tình yêu bé nhỏ của anh à!',
+    introTitle: 'tình yêu bé nhỏ của anhhhhh à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
